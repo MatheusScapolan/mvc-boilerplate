8c29b3330e8d4cd93080ca8498849e6cc10fdc97
MatheusScapolan <matheus8scapolan@gmail.com>
2025-05-13 10:42:28 -0300

    y

---
+++ controllers/contactController.js
@@ -0,0 +1,8 @@
+// Controlador da rota /contato
+exports.index = (req, res) => {
+    res.render('pages/contact', {
+      titulo: 'Contato',
+      mensagem: 'Preencha o formulário abaixo para entrar em contato.'
+    });
+  };
+  
